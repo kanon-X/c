43a0e61041b327bf7bb54be8b8eeab23d41fcc1d
kanon-X <syedur.b@uttarauniversity.edu.bd>
2025-08-07 21:27:29 +0600

    initial commit

---
+++ callbyvalue.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void square(int n);
+
+int main(){
+    int num = 5;
+    square(num);
+    printf("Original number: %d\n", num);
+    return 0;
+}
+
+void square(int n) {
+    n = n * n;
+    printf("Square: %d\n", n);
+}
